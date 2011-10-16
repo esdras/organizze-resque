a6ca013ed3a11c324febfb6fda516aeaea1eee68
Esdras Mayrink <falecom@oesdras.com.br>
2011-10-16 10:57:13 -0200

    removed rails from initializer

--- config/redis.rb
+++ config/redis.rb
@@ -1,6 +1,5 @@
 uri = URI.parse(ENV["REDIS_URL"])
 REDIS_SERVER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-REDIS_NAMESPACE = "organizze:pessoal:#{ENV["REDIS_NAMESPACE"]}:#{Rails.env}"
+REDIS_NAMESPACE = "organizze:pessoal:#{ENV["REDIS_NAMESPACE"]}:production"
 REDIS = Redis::Namespace.new(REDIS_NAMESPACE, :redis => REDIS_SERVER)
 Resque.redis = REDIS
-Rails.logger.error("REDIS OK")
