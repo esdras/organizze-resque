d4a1057aacf455579aeba3394b22c3026b33fe2a
Esdras Mayrink <falecom@oesdras.com.br>
2011-10-28 17:01:32 -0200

    updated redis to run in development

--- config/redis.rb
+++ config/redis.rb
@@ -1,5 +1,5 @@
 uri = URI.parse(ENV["REDIS_URL"])
 REDIS_SERVER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-REDIS_NAMESPACE = "organizze:pessoal:#{ENV["REDIS_NAMESPACE"]}:production"
+REDIS_NAMESPACE = "organizze:pessoal:#{ENV["REDIS_NAMESPACE"]}:#{ENV["RAILS_ENV"] || 'production'}"
 REDIS = Redis::Namespace.new(REDIS_NAMESPACE, :redis => REDIS_SERVER)
 Resque.redis = REDIS
